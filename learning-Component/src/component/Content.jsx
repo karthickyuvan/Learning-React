2d8c0a4d078aacffadf3c5648dec411a37325992
karthickyuvan <alkarthickyuvan@gmail.com>
2025-04-10 17:51:59 +0530

    learning Components and how its works

---
+++ learning-Component/src/component/Content.jsx
@@ -0,0 +1,9 @@
+function Content()
+{
+    return(
+        <div>
+            <h2>This is My First projects in Creating Component</h2>
+        </div>
+    )
+}
+export default Content;
---
+++ learning-Component/src/component/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <div>
+           <p>© 2025 VJ Kudil</p>;
+        </div>
+    )
+}
+export default Footer;
